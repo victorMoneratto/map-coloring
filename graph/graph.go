7b31e774d0bfde373597a90111a0709aa55dc1c2
Victor Moneratto <victor.moneratto@gmail.com>
2015-09-21 22:19:33 -0300

    Optimization (thanks pprof)

--- graph/graph.go
+++ graph/graph.go
@@ -1,17 +1,13 @@
 package graph
 
-import (
-	"errors"
-)
-
 type Graph struct {
-	Map map[string]*Node
+	Nodes []Node
 }
 
 type Node struct {
 	name string
 	C    Color
-	Adj  map[string]*Node
+	Adj  []int
 }
 
 type Color int
@@ -36,55 +32,23 @@ func (c Color) String() string {
 	return ColorNames[c]
 }
 
-func NewNode(name string) *Node {
-	return &Node{name: name}
+func NewNode(name string) Node {
+	return Node{name: name}
 }
 
 func (n Node) Name() string {
 	return n.name
 }
 
-func (g *Graph) GetByName(name string) *Node {
-	return g.Map[name]
-}
-
-func (n *Node) Connect(dest *Node, bidirectional bool) error {
-	if dest == nil {
-		return errors.New("Can't connect to nil node " + dest.Name())
-	}
-
+func (n *Node) Connect(dest int) {
+	const initialCap int = 3
 	if n.Adj == nil {
-		n.Adj = make(map[string]*Node)
+		n.Adj = make([]int, 0, initialCap)
 	}
 
-	n.Adj[dest.Name()] = dest
-	if bidirectional {
-		dest.Connect(n, false)
-	}
-	return nil
+	n.Adj = append(n.Adj, dest)
 }
 
-// Add node to graph by name
-// n is number of neighbors to pre allocate
-func (g *Graph) Add(name string) (*Node, error) {
-
-	if g.Map == nil {
-		g.Map = make(map[string]*Node)
-	}
-
-	node, present := g.Map[name]
-	if present {
-		return node, &DuplicateNodeError{}
-	}
-
-	node = NewNode(name)
-	g.Map[name] = node
-	return node, nil
-}
-
-type DuplicateNodeError struct {
-}
-
-func (e *DuplicateNodeError) Error() string {
-	return "Node was already present"
+func NewGraph(initialCount int) *Graph {
+	return &Graph{Nodes: make([]Node, 0, initialCount)}
 }
--- main.go
+++ main.go
@@ -12,34 +12,27 @@ import (
 	"github.com/victormoneratto/map-coloring/graph"
 )
 
+const (
+	NoHeuristic = 'a' + iota
+)
+
 func main() {
-	heuristics := rune((*flag.String("heuristics", "a", "a, b, c or d"))[0])
+	// cfg := profile.Config{MemProfile: true, ProfilePath: "."}
+	// defer profile.Start(&cfg).Stop()
+
+	//heuristic leve flag
+	heuristicLevel := rune((*flag.String("heuristic", " ", "a, b, c or d"))[0])
 	flag.Parse()
 
-	var g graph.Graph
-	graphDescription := parseInputFile("input/brasil.in", &heuristics)
+	//read file and populate graph
+	graphDescription := parseInputFile("input/usa.in", &heuristicLevel)
+	g := graph.NewGraph(len(graphDescription))
+	populateGraph(g, graphDescription)
 
-	// create all nodes (each is src once)
-	for _, nodeDescription := range graphDescription {
-		src := nodeDescription[0]
-		_, err := g.Add(src)
-		if err != nil {
-			panic(err)
-		}
-	}
-	// connect srcs and dests
-	for _, nodeDescription := range graphDescription {
-		src := g.GetByName(nodeDescription[0])
-		for _, destName := range nodeDescription[1:] {
-			dest := g.GetByName(destName)
-			if err := src.Connect(dest, false); err != nil {
-				panic(err)
-			}
-		}
-	}
-
-	if backtracking(&g) {
-		for _, node := range g.Map {
+	// perform backtracking
+	colorMap(g)
+	if colorMap(g) {
+		for _, node := range g.Nodes {
 			fmt.Println(node.Name()+":", node.C)
 		}
 	} else {
@@ -47,14 +40,87 @@ func main() {
 	}
 }
 
+func populateGraph(g *graph.Graph, graphDescription [][]string) {
+	nodesMap := make(map[string]int)
+
+	// create all nodes (each is src once)
+	for _, nodeDescription := range graphDescription {
+		srcName := nodeDescription[0]
+		node := graph.NewNode(srcName)
+		g.Nodes = append(g.Nodes, node)
+		nodesMap[srcName] = len(g.Nodes) - 1
+	}
+
+	// connect srcs and dests
+	for _, nodeDescription := range graphDescription {
+		src := nodesMap[nodeDescription[0]]
+		for _, destName := range nodeDescription[1:] {
+			dest := nodesMap[destName]
+			g.Nodes[src].Connect(dest)
+		}
+	}
+}
+
+// cache for backtrack
+type BacktrackCache struct {
+	TakenColors []bool
+}
+
+// arrange map coloring backtrack algorithm
+func colorMap(g *graph.Graph) bool {
+	cache := make([]BacktrackCache, len(g.Nodes))
+	for i := 0; i < len(g.Nodes); i++ {
+		cache[i].TakenColors = make([]bool, len(graph.ColorNames))
+	}
+	return colorMapBacktrack(g, cache, 0)
+}
+
+// recursive map coloring (backtrack)
+func colorMapBacktrack(g *graph.Graph, cache []BacktrackCache, height int) bool {
+
+	// have all nodes been colored?
+	if height == len(g.Nodes) {
+		return true
+	}
+
+	takenColors := takenColors(g, cache, height)
+
+	// for each color this node can take
+	for color := graph.Color(graph.Blank + 1); int(color) < len(takenColors); color++ {
+		if !takenColors[color] {
+			g.Nodes[height].C = color
+			if colorMapBacktrack(g, cache, height+1) {
+				return true
+			}
+		}
+	}
+
+	g.Nodes[height].C = graph.Blank
+	return false
+}
+
+// returns array where true represents a taken color, false otherwise
+func takenColors(g *graph.Graph, cache []BacktrackCache, nodeIndex int) []bool {
+	for i := range graph.ColorNames {
+		cache[nodeIndex].TakenColors[i] = false
+	}
+
+	for _, destIndex := range g.Nodes[nodeIndex].Adj {
+		dest := &g.Nodes[destIndex]
+		cache[nodeIndex].TakenColors[dest.C] = true
+	}
+
+	return cache[nodeIndex].TakenColors
+}
+
 // Parse input file to [][]string of the format:
 // [
-//  [src1 dst1 dst2... dstn]
-// 	...
-//  [src2 dst4 dst1... dstm]
+// [src1 dst1 dst2... dstn]
+// ...
+// [src2 dst4 dst1... dstm]
 // ]
 // where each node is src once and many dst can connect to it
-func parseInputFile(filename string, level *rune) [][]string {
+func parseInputFile(filename string, heuristic *rune) [][]string {
 	file, err := os.Open(filename)
 	defer file.Close()
 	if err != nil {
@@ -67,63 +133,26 @@ func parseInputFile(filename string, level *rune) [][]string {
 		panic(err)
 	}
 	fields := strings.Fields(firstLine)
-	count, err := strconv.Atoi(fields[0])
+	nodesCount, err := strconv.Atoi(fields[0])
 	if err != nil {
 		panic(err)
 	}
-	if len(fields) > 1 {
-		*level = rune(fields[1][0])
+	if len(fields) > 1 && *heuristic == ' ' {
+		*heuristic = rune(fields[1][0])
 	}
 
-	ret := make([][]string, count)
+	ret := make([][]string, nodesCount)
 
 	statesExp := regexp.MustCompile("(?:([^,:]*))(?:[,|.|:]) *")
 
-	for i := 0; i < count; i++ {
+	for i := 0; i < nodesCount; i++ {
 		line, _ := buffer.ReadString('\n')
-		for _, name := range statesExp.FindAllStringSubmatch(line, count-1) {
+		submatches := statesExp.FindAllStringSubmatch(line, nodesCount-1)
+		ret[i] = make([]string, 0, len(submatches))
+		for _, name := range submatches {
 			ret[i] = append(ret[i], name[1])
 		}
 	}
 
 	return ret
-}
-
-func backtracking(g *graph.Graph) bool {
-	nodes := make([]*graph.Node, 0, len(g.Map))
-
-	for _, value := range g.Map {
-		nodes = append(nodes, value)
-	}
-
-	return backtrackingRecursive(nodes, 0)
-}
-
-func backtrackingRecursive(nodes []*graph.Node, count int) bool {
-	if count == len(nodes) {
-		return true
-	}
-
-	curr := nodes[count]
-	takenColors := selectColor(curr)
-
-	for color := graph.Color(1); int(color) < len(takenColors); color++ {
-		if !takenColors[color] {
-			curr.C = color
-			if backtrackingRecursive(nodes, count+1) {
-				return true
-			}
-		}
-	}
-	curr.C = graph.Blank
-	return false
-}
-
-func selectColor(node *graph.Node) []bool {
-	takenColors := make([]bool, len(graph.ColorNames))
-	for _, value := range node.Adj {
-		takenColors[value.C] = true
-	}
-
-	return takenColors
 }
